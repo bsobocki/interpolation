029d935c6746917a174025a7b5ac62ef4cabf3d0
bsobocki <bartq98@gmail.com>
2019-12-07 14:26:46 +0100

    rename and add comments

--- code/curve.py
+++ code/curve.py
@@ -2,7 +2,7 @@ import interpolation as intrpltn
 import matplotlib.pyplot as plt
 import img_reader as img_r
 
-def draw_curve(src, num, dim=(600,450)):
+def draw_curve(src, num=1, dim=(600,450)):
     reader = img_r.Img_reader(src)
     fig = plt.figure()
     for i in range(len(reader.objects)):
@@ -10,22 +10,22 @@ def draw_curve(src, num, dim=(600,450)):
             xs = [ a[0] for a in reader.objects[i] ]
             ys = [ a[1] for a in reader.objects[i] ]
 
-            x = []
-            y = []
+            # we can use less points than all pixels
+            x = [xs[i] for i in range(0,len(xs),num)]
+            y = [ys[i] for i in range(0,len(ys),num)]
 
-            for i in range(0,len(xs),num):
-                x.append(xs[i])
-            for i in range(0,len(ys),num):
-                y.append(ys[i])
-
+            # interpolating polynomials
             sx = intrpltn.Interpolation( list( intrpltn.t(len(x)) ), x)
             sy = intrpltn.Interpolation( list( intrpltn.t(len(y)) ), y)
 
+            # points from interpolating polynomial for coordinates
             xx = [sx(c) for c in intrpltn.t(len(x)*1000)]
             yy = [sy(c) for c in intrpltn.t(len(y)*1000)]
 
-            plt.plot(xx,yy)
+            # set point 0,0 on the top left corner
             plt.xlim([0,dim[0]])
             plt.ylim([dim[1],0])
+            
+            plt.plot(xx,yy)
 
     plt.show()
